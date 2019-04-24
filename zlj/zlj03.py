bbd446481e50ab00b4603cf67a677ecb95c0d5a5
zlj913111 <18601209320@163.com>
2019-04-24 16:20:59 +0800

    算法

---
+++ zlj/zlj03.py
@@ -0,0 +1,9 @@
+a = 4.0
+b = 3.0
+print (" %s + %s = %s" % (a,b, str(a + b)))
+print (" %s - %s = %s" % (a,b, str(a - b)))
+print (" %s * %s = %s" % (a,b, str(a * b)))
+print (" %s / %s = %s" % (a,b, str(a / b)))
+# print (" %s % %s = %s" % (a,b, str(a % b)))
+
+print(str(a)+ " % " +str(b) + "=" +str(a+b))
