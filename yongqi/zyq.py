4ab5306418dab1f3bd6c4dec1891c670fcc6a078
yongqi <1301918819@qq.com>
2019-04-24 16:27:28 +0800

    删除多余文件

--- yongqi/zyq.py
+++ yongqi/zyq.py
@@ -1,9 +1,9 @@
 #-*- encoding:utf-8 -*-
 year=int(raw_input("请输入年份"))
 if year % 4 == 0 and year%100!= 0:
-    print ("%s是普通闰年" % year)
+    print ("%s普通闰年" % year)
 elif year % 400 == 0 and year != 0:
-    print ("%s是世界闰年" % year)
+    print ("%s世界闰年" % year)
 elif year <= 0:
     print  ("请输入正确的年份")
 else:
