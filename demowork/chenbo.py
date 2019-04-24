f0176ed402695a79a548898f69e9114c000dd646
chenbo <2810413349@qq.com>
2019-04-24 16:17:50 +0800

    上传

--- demowork/chenbo.py
+++ demowork/chenbo.py
@@ -1,3 +1,10 @@
+# -*- encoding:utf-8 -*-
 
-print("hello world.")
-print("")
+year=int(raw_input("请输入一个年份："))
+if((year%4==0) and (year%100!=0)):
+    print(str(year)+"是普通闰年。")
+elif(year%400==0):
+    print(str(year)+"是世纪闰年。")
+
+else:
+    print(str(year)+"不是闰年。")
