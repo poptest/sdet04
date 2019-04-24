9e4c781638aadff74100d92f61cdc211f2aa6bbd
wzyuchen <896417651@qq.com>
2019-04-24 16:26:13 +0800

    王震的闰年判定

--- WZ/wztest02.py
+++ WZ/wztest02.py
@@ -1,6 +1,5 @@
 # -*- encoding:utf-8 -*-
-year = int(raw_input("请输入年份："))
-
+year = int(raw_input("请输入整数型的年份："))
 if year>=0:
     if year%4==0and year%100!=0:
         print "公元"+str(year)+"年是普通闰年"
