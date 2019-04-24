d28442aa0e2645bd0c0cf8f5b110ebcd5e76477d
GaoLinCong79 <1690344964@qq.com>
2019-04-24 15:58:56 +0800

    闰年

---
+++ Demo_glc01/demo04.py
@@ -0,0 +1,12 @@
+# -*-encoding:utf-8-*-
+year=int(raw_input ("请输入年份："))
+# if year.is_float:
+#     print (str(year)+"是小数请重新输入")
+if year%4==0 and year%100==0:
+    print (str(year)+"是普通闰年")
+elif year%400==0:
+    print (str(year)+"是世纪闰年")
+elif year>0:
+    print (str(year)+"是平年")
+else:
+    print ("请输入正确的年份")
