0a24aa52edb2e34f4a649b30c7a40fa14621c0c0
1099415725 <jiaweizhang898@163.com>
2019-04-24 17:10:35 +0800

    nnj

--- sdet0424.py
+++ sdet0424.py
@@ -1,8 +1,15 @@
 #encoding:utf-8
+
 year=int(raw_input("请输入年份："))
+
 if year%4==0 and year%100!=0:
+
     print("%s是普通闰年"%year)
+
 elif year%400==0:
+
     print("%s是超级闰年"%year)
+
 else:
+
     print("%s不是闰年"%year)
